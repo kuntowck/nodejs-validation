0b3b702a2c9cc29cabbd77474d8b94045d44eb46
kuntowck <kunto312002@gmail.com>
2024-07-11 08:28:56 +0700

    create nested object vallidation schema

--- tests/validation-error.test.js
+++ tests/validation-error.test.js
@@ -5,7 +5,7 @@ describe("Joi", () => {
   it("should return validation error", () => {
     const schema = Joi.string().email().min(3).required();
 
-    const result = schema.validate("ow", { abortEarly: false });
+    const result = schema.validate("ow", { abortEarly: false }); // stops validation on the first error and return all the errors
     console.log(result.value);
 
     if (result.error) {
--- tests/validation-object.test.js
+++ tests/validation-object.test.js
@@ -5,7 +5,7 @@ describe("Joi", () => {
   it("should can validate object schema", () => {
     const loginSchema = Joi.object({
       username: Joi.string().email().min(3).max(20).required(),
-      password: Joi.string().min(6).max(20).required()
+      password: Joi.string().min(6).max(20).required(),
     });
 
     const request = {
@@ -16,5 +16,31 @@ describe("Joi", () => {
     const result = loginSchema.validate(request, { abortEarly: false });
 
     console.log(result.value);
+  });
+
+  it("should can validate nested object schema", () => {
+    const createUserSchema = Joi.object({
+      id: Joi.string().max(20).required(),
+      password: Joi.string().max(20).required(),
+      address: Joi.object({
+        street: Joi.string().max(200).required(),
+        city: Joi.string().max(100).required(),
+        country: Joi.string().max(100).required(),
+        zipCode: Joi.string().max(10).required(),
+      }).required(),
+    });
+
+    const request = {
+      id: "kuntowck",
+      password: "rahasia",
+      address: {},
+    };
+
+    const result = createUserSchema.validate(request, { abortEarly: false });
+
+    console.log(result.value);
+    if (result.error) {
+      result.error.details.map((e) => console.log(`${e.path}: ${e.message}`));
+    }
   });
 });
