1e22169622e27ef8f7a82d7104d8b578a06ad5ff
kuntowck <kunto312002@gmail.com>
2024-07-17 08:35:51 +0700

    create array of object validation schema

--- tests/array.test.js
+++ tests/array.test.js
@@ -14,4 +14,28 @@ describe("Joi", () => {
 
     console.log(result);
   });
+
+  it("should can validate array of object", () => {
+    const schema = Joi.array()
+      .min(1)
+      .items(
+        Joi.object({
+          street: Joi.string().required().max(200),
+          city: Joi.string().required().max(100),
+          country: Joi.string().required().max(100),
+        })
+      );
+
+    const address = [
+      {
+        street: "Jl.Asmawi H.M",
+        city: "Tangerang Selatan",
+        country: "Indonesia",
+      },
+    ];
+
+    const result = schema.validate(address, { abortEarly: false });
+    console.log(result.value);
+    console.log(result.error);
+  });
 });
